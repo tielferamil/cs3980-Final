bc3ad589338b9a9c357b65a976781b7624e47b05
feramil <sealtiel-feramil@uiowa.edu>
2025-04-22 11:47:06 -0500

    trouble shooting database

--- database.py
+++ database.py
@@ -1,7 +1,18 @@
 from pymongo import MongoClient
+from database import users_collection
+from fastapi import HTTPException
 
 client = MongoClient(
     "mongodb+srv://tielferamil:KHp0Wvip8ohKtawk@cluster0.9mh9yk6.mongodb.net/"
 )
 db = client["nutritrack"]
 users_collection = db["users"]
+
+
+@app.get("/test-db")
+def test_db_connection():
+    try:
+        count = users_collection.count_documents({})
+        return {"status": "success", "user_count": count}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
