1551402d17c273374fa07fad250c90b88bfe68da
feramil <sealtiel-feramil@uiowa.edu>
2025-04-22 11:43:16 -0500

    database fix maybe

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 client = MongoClient(
-    "mongodb+srv://tielferamil:KHp0Wvip8ohKtawk@cluster0.xxxxx.mongodb.net/nutritrack?retryWrites=true&w=majority"
+    "mongodb+srv://tielferamil:KHp0Wvip8ohKtawk@cluster0.9mh9yk6.mongodb.net/"
 )
 db = client["nutritrack"]
 users_collection = db["users"]
