a5af488d546d39ab3c761a7ca751234fe8eb721d
feramil <sealtiel-feramil@uiowa.edu>
2025-04-27 19:33:28 -0500

    signup/logout

--- database.py
+++ database.py
@@ -5,12 +5,14 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from models import User
 
 client = MongoClient(
-    "mongodb+srv://tielferamil:KHp0Wvip8ohKtawk@cluster0.9mh9yk6.mongodb.net/"
+    "mongodb+srv://tielferamil:KHp0Wvip8ohKtawk@cluster0.9mh9yk6.mongodb.net/nutritrack?retryWrites=true&w=majority"
 )
 db = client["nutritrack"]
 users_collection = db["users"]
 
 
 async def init_db():
-    client = AsyncIOMotorClient("mongodb+srv://joewroble72:y7pbeqm5liMmC1xV@cluster0.9mh9yk6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    client = AsyncIOMotorClient(
+        "mongodb+srv://joewroble72:y7pbeqm5liMmC1xV@cluster0.9mh9yk6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    )
     await init_beanie(database=client.nutritrack, document_models=[User])
